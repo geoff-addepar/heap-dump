a3237134674eccd6eaf2166e92483ba3d11811ff
Geoff Lywood <geoff@addepar.com>
2016-09-14 12:11:17 -0700

    WIP

--- src/com/addepar/heapdump/debugger/ElfSymbolLookup.java
+++ src/com/addepar/heapdump/debugger/ElfSymbolLookup.java
@@ -1,6 +1,8 @@
 package com.addepar.heapdump.debugger;
 
+import java.io.IOException;
 import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
 
 /**
  * Utility to help find non-dynamic symbols in an ELF file
@@ -10,11 +12,11 @@ import java.nio.channels.FileChannel;
 public class ElfSymbolLookup {
   private final FileChannel fileChannel;
 
-  public ElfSymbolLookup(String filename) {
-    this.fileChannel =
+  public ElfSymbolLookup(String filename) throws IOException {
+    this.fileChannel = FileChannel.open(Paths.get(filename));
   }
 
-  public void close() {
+  public void close() throws IOException {
     fileChannel.close();
   }
 }
